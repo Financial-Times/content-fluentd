ee9ffe4be5f217a8ff212deeb13071d2f7fb4c00
Donislav Belev <donislav.belev@ft.com>
2021-11-04 11:47:24 +0200

    Fluentd verbose remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec fluentd -vv -c /fluentd/etc/${FLUENTD_CONF} --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
+exec fluentd -c /fluentd/etc/${FLUENTD_CONF} --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
